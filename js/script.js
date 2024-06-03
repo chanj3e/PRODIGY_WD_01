5d7cb44e7436873b6c0a492242ce458eaea96ef3
omar atef <change0m0@gmail.com>
2024-06-03 18:55:17 +0300

    lovely edit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 
 const nav = document.querySelector("#nav");
 const navItems = document.querySelectorAll(".nav-item");
+const header = document.querySelector("header");
+const active = document.querySelector(".active");
+const navImg = document.querySelector(".nav-img");
 const verifyNav = document.querySelector;
 
 nav.addEventListener("click", function (e) {
@@ -12,3 +15,18 @@ nav.addEventListener("click", function (e) {
   navItems.forEach((item) => item.classList.remove("active"));
   clicked.classList.add("active");
 });
+
+const sticky = header.offsetTop;
+window.onscroll = function () {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    navItems.forEach((i) => (i.style.color = "#212b36"));
+    active.style.color = "blue";
+    navImg.src = "/images/logo-2.png";
+  } else {
+    header.classList.remove("sticky");
+    navItems.forEach((i) => (i.style.color = "white"));
+    active.style.color = "wheat";
+    navImg.src = "/images/logo.png";
+  }
+};
